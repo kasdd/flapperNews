a32dc99d57066e3d559ecc1fe772fce23c7405e8
Kas De Durpel <kas_dedurpel@hotmail.com>
2016-10-18 09:51:01 +0200

    Angular done

--- Part 1/app.js
+++ Part 1/app.js
@@ -1,19 +1,26 @@
 var app = angular.module('flapperNews', ['ui.router']);
 
 app.config([
-'$stateProvider',
-'$urlRouterProvider',
-function($stateProvider, $urlRouterProvider) {
+    '$stateProvider',
+    '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
 
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: '/home.html',
-      controller: 'MainCtrl'
-    });
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: '/home.html',
+                controller: 'MainCtrl'
+            });
 
-  $urlRouterProvider.otherwise('home');
-}]);
+        $stateProvider.state('posts', {
+            url: '/posts/{id}',
+            templateUrl: '/posts.html',
+            controller: 'PostsCtrl'
+        });
+
+        $urlRouterProvider.otherwise('home');
+    }
+]);
 
 app.factory('posts', [function () {
     var o = {
@@ -53,7 +60,10 @@ app.controller('MainCtrl', [
                 title: $scope.title,
                 link: $scope.link,
                 upvotes: 0,
-
+                comments : [
+                    {author : 'Joe', body: 'Cool post!', upvotes: 0},
+                    {author : 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+                ]
             });
             $scope.title = '';
             $scope.link = '';
@@ -61,5 +71,23 @@ app.controller('MainCtrl', [
         $scope.incrementUpvotes = function (post) {
             post.upvotes += 1;
         };
+    }
+]);
+
+app.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function ($scope, $stateParams, posts) {
+        $scope.post = posts.posts[$stateParams.id];
+    },
+    $scope.addComment = function(){
+        if($scope.body === '') {return; }
+        $scope.post.comments.push({
+            body: $scope.body, 
+            author: 'user', 
+            upvotes: 0
+        });
+        $scope.body = '';
     }
 ]);
