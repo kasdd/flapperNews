405a8f4d64a3ffb20205a7682833e288936f5079
Kas De Durpel <kas_dedurpel@hotmail.com>
2016-12-15 05:42:33 +0100

    Laatste commit

--- Part 2/flapper-news/public/javascripts/angularApp.js
+++ Part 2/flapper-news/public/javascripts/angularApp.js
@@ -1,44 +1,40 @@
-var app = angular.module('flapperNews', ['ui.router']);
+angular.module('flapperNews').config(flapperNewsStates);
 
-app.config([
-    '$stateProvider',
-    '$urlRouterProvider',
-    function ($stateProvider, $urlRouterProvider) {
+flapperNewsStates.$inject = ['$stateProvider', '$urlRouterProvider'];
 
-        $stateProvider.state('home', {
+function flapperNewsStates($stateProvider, $urlRouterProvider){
+    $stateProvider.state('home', {
             url: '/home',
             templateUrl: '/home.html',
-            controller: 'MainCtrl',
-            resolve: {
-                postPromise: ['posts', function (posts) {
-                    return posts.getAll();
-                }]
-            }
+            controller: 'MainController',
+            controllerAs: 'ctrl'
         }).state('posts', { //Wat we hier definieeren geven we door tussen onze states (in dit geval het url (id) om de juiste post terug te geven)
             url: '/posts/:id',
             templateUrl: '/posts.html',
-            controller: 'PostsCtrl',
+            controller: 'PostController',
+            controllerAs: 'ctrl',
             resolve: {
-                post: ['$stateParams', 'posts',
-                    function ($stateParams, posts) {
-                        return posts.get($stateParams.id);
+                post: ['$stateParams', 'postService',
+                    function ($stateParams, postService) {
+                        return postService.get($stateParams.id);
                     }]
             }
         }).state('login', {
             url: '/login',
             templateUrl: '/login.html',
-            controller: 'AuthCtrl',
+            controller: 'AuthController',
+            controllerAs: 'ctrl',
             onEnter: ['$state', 'auth',
                 function ($state, auth) {
                     if (auth.isLoggedIn()) {  //Wanneer ingelogd gaan we naar homestate
                         $state.go('home');
                     }
                 }]
-
         }).state('register', {
             url: '/register',
             templateUrl: '/register.html',
-            controller: 'AuthCtrl',
+            controller: 'AuthController',
+            controllerAs: 'ctrl',
             onEnter: ['$state', 'auth',
                 function ($state, auth) {
                     if (auth.isLoggedIn()) { //Wanneer geregistreerd gaan we naar homestate
@@ -47,227 +43,5 @@ app.config([
                 }]
         });
 
-
         $urlRouterProvider.otherwise('home');
-    }
-]);
-
-app.factory('auth', ['$http', '$window', function ($http, $window) {
-    var auth = {};
-
-    auth.saveToken = function (token) {
-        $window.localStorage['flapper-news-token'] = token;
-    };
-
-    auth.getToken = function () {
-        return $window.localStorage['flapper-news-token'];
-    }
-
-    auth.isLoggedIn = function () {
-        var token = auth.getToken();
-        //Als de token bestaat moeten we kijken of de payload nog niet expired is.
-        if (token) {
-            var payload = JSON.parse($window.atob(token.split('.')[1]));
-            return payload.exp > Date.now() / 1000;
-        } else {
-            return false;
-        }
-    };
-    //Geeft username terug die ingelogd is
-    auth.currentUser = function () {
-        if (auth.isLoggedIn()) {
-            var token = auth.getToken();
-            var payload = JSON.parse($window.atob(token.split('.')[1]));
-
-            return payload.username;
-        }
-    };
-
-    auth.register = function (user) {
-        return $http.post('/register', user).success(function (data) {
-            auth.saveToken(data.token);
-        });
-    };
-
-    auth.logIn = function (user) {
-        return $http.post('/login', user).success(function (data) {
-            auth.saveToken(data.token);
-        });
-    };
-
-    auth.logOut = function () {
-        $window.localStorage.removeItem('flapper-news-token');
-    };
-
-
-    return auth;
-}]);
-
-/* We creeeren een nieuw object dat een property posts heeft. 
-We geven dan die variabele (o) terug zodat deze door elke angular module kan gebruikt worden. 
-Je kan hier ook methode in exporteren
-$scope.posts = posts.posts in MainCtrl --> Nu wordt elke wijziging opgeslaan in de service*/
-app.factory('posts', ['$http', 'auth', function ($http, auth) {
-    var o = {
-        posts: []
-    };
-
-    o.getAll = function () {
-        return $http.get('/posts').success(function (data) {
-            angular.copy(data, o.posts);
-        });
-    }
-
-    o.create = function (post) {
-        return $http.post('/posts', post, {
-            headers: {
-                Authorization: 'Bearer ' + auth.getToken()
-            }
-        }).success(function (data) {
-            o.posts.push(data);
-        });
-    };
-
-    o.upvote = function (post) {
-        return $http.put('/posts/' + post._id + '/upvote', null, {
-            headers: {
-                Authorization: 'Bearer ' + auth.getToken()
-            }
-        }).success(function (data) {
-            post.upvotes += 1;
-        });
-    };
-
-    o.downvote = function(post){
-        return $http.put('/posts/' + post._id + '/downvote', null, {
-            headers : {
-                Authorization : 'Bearer ' + auth.getToken()
-            }
-        }).success(function(data){
-            post.upvotes -= 1;
-        })
-    }
-
-    o.get = function (id) {
-        return $http.get('/posts/' + id).then(function (res) {
-            return res.data;
-        });
-    };
-
-    o.addComment = function (id, comment) {
-        return $http.post('/posts/' + id + '/comments', comment, {
-            headers: {
-                Authorization: 'Bearer ' + auth.getToken()
-            }
-        });
-    };
-
-    o.upvoteComment = function (post, comment) {
-        return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote', null, {
-            headers: {
-                Authorization: 'Bearer ' + auth.getToken()
-            }
-        }).success(function (data) {
-            comment.upvotes += 1;
-        });
-    };
-
-    o.downvoteComment = function (post, comment) {
-        return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/downvote', null, {
-            headers: {
-                Authorization: 'Bearer ' + auth.getToken()
-            }
-        }).success(function (data) {
-            comment.upvotes -= 1;
-        });
-    };
-
-    return o;
-}]);
-
-app.controller('MainCtrl', [
-    '$scope', 'posts', 'auth', 
-    function ($scope, posts, auth) {
-        $scope.posts = posts.posts
-        $scope.isLoggedIn = auth.isLoggedIn;
-
-        $scope.addPost = function () {
-            if ($scope.title === '') {
-                return;
-            }
-            posts.create({
-                title: $scope.title,
-                link: $scope.link,
-            });
-            //De $scope items leegmaken (anders blijven ze staan na submit)
-            $scope.title = '';
-            $scope.link = '';
-        };
-
-        $scope.incrementUpvotes = function (post) {
-            posts.upvote(post);
-        };
-
-        $scope.incrementDownvotes = function(post){
-            posts.downvote(post);
-        }
-    }
-]);
-
-app.controller('PostsCtrl', ['$scope', 'posts', 'post', 'auth',
-    function ($scope, posts, post, auth) {
-        $scope.post = post;
-        $scope.isLoggedIn = auth.isLoggedIn;
-
-        $scope.addComment = function () {
-            if ($scope.body === '') {
-                return;
-            }       
-            posts.addComment(post._id, {
-                body: $scope.body,
-                author: 'user'
-            }).success(function (comment) {
-                $scope.post.comments.push(comment);
-            });
-            $scope.body = '';
-        };
-        $scope.upvote = function (comment) {
-            posts.upvoteComment(post, comment);
-        };
-        $scope.downvote = function (comment) {
-            posts.downvoteComment(post, comment);
-        };
-    }
-]);
-
-app.controller('AuthCtrl', [
-    '$scope', '$state', 'auth',
-    function ($scope, $state, auth) {
-        $scope.user = {};
-
-        $scope.register = function () {
-            auth.register($scope.user).error(function (error) {
-                $scope.error = error;
-            }).then(function () {
-                $state.go('home');
-            });
-        };
-
-        $scope.logIn = function () {
-            auth.logIn($scope.user).error(function (error) {
-                $scope.error = error;
-            }).then(function () {
-                $state.go('home');
-            });
-        };
-    }
-]);
-
-app.controller('NavCtrl', [
-    '$scope', 'auth',
-    function ($scope, auth) {
-        $scope.isLoggedIn = auth.isLoggedIn;
-        $scope.currentUser = auth.currentUser;
-        $scope.logOut = auth.logOut;
-    }
-]);
+}
--- Part 2/flapper-news/routes/index.js
+++ Part 2/flapper-news/routes/index.js
@@ -9,129 +9,173 @@ var Comment = mongoose.model('Comment');
 var User = mongoose.model('User');
 
 //De optie userProperty wordt gebruikt om de property req te specificeren (user by default, wij payload)
-var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
+var auth = jwt({
+  secret: 'SECRET',
+  userProperty: 'payload'
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
 module.exports = router;
 
 /* Geeft JSON terug met alle posts */
-router.get('/posts', function(req, res, next) {
-  Post.find(function(err, posts){
-    if(err){ return next(err); }
+router.get('/posts', function (req, res, next) {
+  Post.find(function (err, posts) {
+    if (err) {
+      return next(err);
+    }
 
     res.json(posts);
   });
 });
 
 /* Om data in onze database te steken */
-router.post('/posts', auth, function(req, res, next) {
+router.post('/posts', auth, function (req, res, next) {
   var post = new Post(req.body);
   post.author = req.payload.username;
-  
-  post.save(function(err, post){
-    if(err){ return next(err); }
+
+  post.save(function (err, post) {
+    if (err) {
+      return next(err);
+    }
 
     res.json(post);
   });
 });
 
 /* Laadt automatisch een object, als :post in router path staat gaan we onderstaande automatisch uitvoeren */
-router.param('post', function(req, res, next, id) {
+router.param('post', function (req, res, next, id) {
   var query = Post.findById(id);
-  query.exec(function (err, post){
-    if (err) { return next(err); }
-    if (!post) { return next(new Error("can't find post")); }
+  query.exec(function (err, post) {
+    if (err) {
+      return next(err);
+    }
+    if (!post) {
+      return next(new Error("can't find post"));
+    }
     req.post = post;
     return next();
   });
 });
 
 /* Laadt automatisch een object, als :comment in router path staat gaan we onderstaande automatisch uitvoeren */
-router.param('comment', function(req, res, next, id) {
+router.param('comment', function (req, res, next, id) {
   var query = Comment.findById(id);
 
-  query.exec(function (err, comment){
-    if (err) { return next(err); }
-    if (!comment) { return next(new Error("can't find comment")); }
+  query.exec(function (err, comment) {
+    if (err) {
+      return next(err);
+    }
+    if (!comment) {
+      return next(new Error("can't find comment"));
+    }
 
     req.comment = comment;
     return next();
   });
 });
 
-/* Gaat automatisch alle comments laden die met deze post te maken heeft */
-router.get('/posts/:post', function(req, res, next) {
-  req.post.populate('comments', function(err, post) {
+router.get('/posts/:post', function (req, res, next) {
+  req.post.populate('comments', function (err, post) { /* Gaat automatisch alle comments laden die met deze post te maken heeft */
     res.json(post);
   });
 });
 
-router.put('/posts/:post/upvote', auth, function(req,res, next){
-  req.post.upvote(function(err,post){
-    if(err){return next(err);}
+router.put('/posts/:post/upvote', auth, function (req, res, next) {
+  req.post.upvote(function (err, post) {
+    if (err) {
+      return next(err);
+    }
 
     res.json(post);
   })
 });
 
-router.put('/posts/:post/downvote', auth, function(req,res, next){
-  req.post.downvote(function(err,post){
-    if(err){return next(err);}
+router.put('/posts/:post/downvote', auth, function (req, res, next) {
+  req.post.downvote(function (err, post) {
+    if (err) {
+      return next(err);
+    }
     res.json(post);
   })
 });
 
-router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
-  req.comment.upvote(function(err, comment){
-    if (err) { return next(err); }
+router.put('/posts/:post/comments/:comment/upvote', auth, function (req, res, next) {
+  req.comment.upvote(function (err, comment) {
+    if (err) {
+      return next(err);
+    }
 
     res.json(comment);
   });
 });
 
+router.delete('/posts/:post/comments/:comment', auth, function(req, res, next) {
+    Comment.remove({_id: req.comment._id}, function(err, comment) {
+        if (err) {
+            res.send(err);
+        }
+        res.json({
+            message: 'comment deleted'
+        });
+    });
+});
 
-router.put('/posts/:post/downvote', auth, function(req, res, next) {
-  req.post.downvote(function(err, post){
-    if (err) { return next(err); }
+
+
+router.put('/posts/:post/downvote', auth, function (req, res, next) {
+  req.post.downvote(function (err, post) {
+    if (err) {
+      return next(err);
+    }
 
     res.json(post);
   });
 });
 
-router.post('/posts/:post/comments', auth, function(req, res, next) {
+router.post('/posts/:post/comments', auth, function (req, res, next) {
   var comment = new Comment(req.body);
   comment.post = req.post;
   comment.author = req.payload.username;
 
-  comment.save(function(err, comment){
-    if(err){ return next(err); }
+  comment.save(function (err, comment) {
+    if (err) {
+      return next(err);
+    }
 
     req.post.comments.push(comment);
-    req.post.save(function(err, post) {
-      if(err){ return next(err); }
+    req.post.save(function (err, post) {
+      if (err) {
+        return next(err);
+      }
 
       res.json(comment);
     });
   });
 });
 
-router.put('/posts/:post/comments/:comment/downvote', auth, function(req, res, next) {
-     console.log('hellodown');
-     console.log(req.comment);
-  req.comment.downvote(function(err, comment){
-    if (err) { return next(err); }
+router.put('/posts/:post/comments/:comment/downvote', auth, function (req, res, next) {
+  console.log('hellodown');
+  console.log(req.comment);
+  req.comment.downvote(function (err, comment) {
+    if (err) {
+      return next(err);
+    }
 
     res.json(comment);
   });
 });
 
-router.post('/register', function(req, res, next){
-  if(!req.body.username || !req.body.password){
-    return res.status(400).json({message: 'Please fill out all fields'});
+router.post('/register', function (req, res, next) {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({
+      message: 'Please fill out all fields'
+    });
   }
 
   var user = new User();
@@ -140,23 +184,33 @@ router.post('/register', function(req, res, next){
 
   user.setPassword(req.body.password)
 
-  user.save(function (err){
-    if(err){ return next(err); }
+  user.save(function (err) {
+    if (err) {
+      return next(err);
+    }
 
-    return res.json({token: user.generateJWT()})
+    return res.json({
+      token: user.generateJWT()
+    })
   });
 });
 
-router.post('/login', function(req, res, next){
-  if(!req.body.username || !req.body.password){
-    return res.status(400).json({message: 'Please fill out all fields'});
+router.post('/login', function (req, res, next) {
+  if (!req.body.username || !req.body.password) {
+    return res.status(400).json({
+      message: 'Please fill out all fields'
+    });
   }
 
-  passport.authenticate('local', function(err, user, info){
-    if(err){ return next(err); }
+  passport.authenticate('local', function (err, user, info) {
+    if (err) {
+      return next(err);
+    }
 
-    if(user){
-      return res.json({token: user.generateJWT()});
+    if (user) {
+      return res.json({
+        token: user.generateJWT()
+      });
     } else {
       return res.status(401).json(info);
     }
